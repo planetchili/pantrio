4b3bd3d326d04a8ad37f1a00a002357b792a4501
chili <ray_kun@hotmail.com>
2021-02-12 22:15:15 +0900

    sexify some layout/style

--- resources/nuxt/pages/index.vue
+++ resources/nuxt/pages/index.vue
@@ -4,14 +4,14 @@
             <v-expansion-panels>
                 <v-expansion-panel v-for="(section,i) in sections" :key="i">
                     <v-expansion-panel-header>
-                        {{section.name}}
+                        <h2>{{section.name}}</h2>
                     </v-expansion-panel-header>
                     <v-expansion-panel-content>
                         <v-card v-for="(item,j) in section.items" :key="j">
                             <v-card-text>
-                                <v-row no-gutters>
-                                    <v-col :cols="9"><h2>{{item.name}}</h2></v-col>
-                                    <v-col :cols="3"><v-text-field label="QTY" :value="item.qty" outlined></v-text-field></v-col>
+                                <v-row no-gutters align="center">
+                                    <v-col :cols="9"><h3>{{item.name}}</h3></v-col>
+                                    <v-col :cols="3"><v-text-field label="QTY" :value="item.qty" dense hide-details outlined></v-text-field></v-col>
                                 </v-row>
                             </v-card-text>
                         </v-card>
